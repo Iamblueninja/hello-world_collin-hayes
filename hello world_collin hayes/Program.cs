895ead7f78af7aaf1322cb4b2346a939395e1f77
Iamblueninja <youcancallmecollin@gmail.com>
2025-02-26 10:04:34 -0600

    Add project files.

---
+++ hello world_collin hayes/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HelloWorld_Collin_Hayes
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name?");  // Asks the user for first name
+            string FirstName = Console.ReadLine();          // Assigns user Input to FirtsName variable
+            Console.WriteLine("What is your last name?");   // Asks the user for last name
+            string lastname = Console.ReadLine();           // Assigns user input to lastname variable
+
+            Console.WriteLine($"Hello, {FirstName} {lastname}!");   // prints a welcome home message with FirstName and lastname
+        }
+    }
+}
